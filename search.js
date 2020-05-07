15b3eda67836921ff4a9960108da5f717cd93d19
Maczi01 <mateusz.w.twardy@gmail.com>
2020-05-07 17:09:05 +0200

    add search in books by title and description

--- search.js
+++ search.js
@@ -1,11 +1,28 @@
+const lunr = require("lunr");
+
+
+function initBooks(db) {
+    return lunr(function () {
+        this.ref('id')
+        this.field('title')
+        this.field('description')
+        db.getAllBooks().forEach((book) => {
+            this.add(book)
+        }, this)
+    })
+}
+
+
 class Search {
     constructor(db) {
         this.db = db
+        this.booksIndex = initBooks(this.db)
     }
 
     findBooks(searchQuery) {
-        const foundIds = ["1", "2"]
-        return foundIds.map(this.db.getBookById)
+        const results = this.booksIndex.search(searchQuery)
+        const foundsId = results.map(res => res.ref);
+        return foundsId.map(this.db.getBookById);
     }
 }
 
