ac1b1f2094d6d31047c9289473c3a860be154454
Maczi01 <mateusz.w.twardy@gmail.com>
2020-06-30 00:00:57 +0200

    add delete function

--- resolvers.js
+++ resolvers.js
@@ -73,6 +73,10 @@ const resolvers = {
             updateUser: (rootValue, {id, name, info}, {db}) => {
                 db.updateUser(toDbId(id), {name, info})
                 return db.getUserById(toDbId(id))
+            },
+            deleteUser: (rootValue, {id}, {db}) => {
+                db.deleteUser(toDbId(id))
+                return id;
             }
         },
         Book: {
